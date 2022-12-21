85217a0dd6795edc828d55575dc2599e6bb07ea7
VVilchak <valyavilchak@gmail.com>
2022-12-21 13:24:00 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c14774b1-f9b0-4efb-b4f8-5771799fdfa5';
+export const MODAL_CLOSE_SEC = 2.5;
